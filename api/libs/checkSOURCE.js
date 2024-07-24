6010f551ac08d2bb458d1e34ca2b9bb61cb7b49f
tricampeonsite <tricampeonsite@gmail.com>
2024-07-23 23:30:47 -0300

    add node-fetch in checkSource.js

--- api/libs/checkSOURCE.js
+++ api/libs/checkSOURCE.js
@@ -1,14 +1,18 @@
-import axios from 'axios';
+import fetch from "node-fetch";
 
 // @param url = ruta de transmision
 // @param callback = callback(success)
-
 export default async (url, callback) => {
     try {
+        const response = await fetch(url, { timeout: 15000 });
 
-        callback(true);
+        if (response.ok) {
+            callback(true);
+        } else {
+            console.log('Error:', response.status);
+            callback(false);
+        }
     } catch (error) {
-
         callback(false);
     }
 };
