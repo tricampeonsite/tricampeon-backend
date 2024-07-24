0703cc29fed5260bcadf89260c3bd8ac73087753
tricampeonsite <tricampeonsite@gmail.com>
2024-07-23 21:43:49 -0300

    testing checkSOURCE.js

--- api/libs/checkSOURCE.js
+++ api/libs/checkSOURCE.js
@@ -4,11 +4,20 @@ import axios from 'axios';
 // @param callback = callback(success)
 export default async (url, callback) => {
     try {
-        const req = await axios.get(url)
+        const req = await axios.get(url);
         const { status } = req;
         callback(status === 200 || status == 302 || status === 304);
     } catch (error) {
-        console.log("ERROR EN CHECKSOURCE: ", error);
+        if (error.response) {
+            // La solicitud fue realizada y el servidor respondió con un estado de error
+            console.log("ERROR EN CHECKSOURCE: ", error.response.status);
+        } else if (error.request) {
+            // La solicitud fue realizada pero no se recibió respuesta
+            console.log("ERROR EN CHECKSOURCE: No se recibió respuesta", error.request);
+        } else {
+            // Algo ocurrió al configurar la solicitud
+            console.log("ERROR EN CHECKSOURCE: ", error.message);
+        }
         callback(false);
     }
 };
