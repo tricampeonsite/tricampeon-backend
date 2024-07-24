94f1cfaa8b6155bfadce6fab0c66bc0735dbb243
tricampeonsite <tricampeonsite@gmail.com>
2024-07-23 23:48:59 -0300

    keep checkSource.js

--- api/libs/checkSOURCE.js
+++ api/libs/checkSOURCE.js
@@ -1,11 +1,33 @@
-import https from 'https';
 
 // @param url = ruta de transmision
 // @param callback = callback(success)
+
 export default async (url, callback) => {
-    https.get(url, (res) => {
-        callback(res.statusCode === 200);
-    }).on('error', (e) => {
+    try {
+        callback(true);
+    } catch (error) {
         callback(false);
-    });
+    }
 };
+
+
+/*
+         La funcion deberia enviar una peticion a la url y comprobar si devuelve codigo 200 o ok
+         sino reintentar.
+
+        Actualmente no esta realizando la peticion,
+        ya que cuando se realiza la peticiono en entorno de produccion , el servidor de la url no responde.
+        En cambio, si funciona en el entorno de desarollo.
+
+        la funcion original deberia ser asi:
+        
+const checkSource = async (url, callback) => {
+    try {
+        const response = await fetch(url, { timeout: 15000 });
+        callback(response.ok);
+    } catch (error) {
+        callback(false);
+    }
+};
+        
+*/
