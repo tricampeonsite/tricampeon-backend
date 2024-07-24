6ae0ea7472901c1a96d60a33be20b28d3dbc6aba
tricampeonsite <tricampeonsite@gmail.com>
2024-07-23 22:54:52 -0300

    testeando checkSources.js

--- api/libs/checkSOURCE.js
+++ api/libs/checkSOURCE.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
-import https from 'https';
 
 // @param url = ruta de transmision
 // @param callback = callback(success)
 export default async (url, callback) => {
     try {
-        // Configura Axios con un timeout más largo y un agente HTTPS si es necesario
-        const agent = new https.Agent({ rejectUnauthorized: false });
-        const req = await axios.get(url, { timeout: 15000, httpsAgent: agent }); // Timeout de 15 segundos
+        const req = await axios.get(url, {
+            headers: {
+                'Access-Control-Allow-Origin': '*', // Puedes ajustar esto según el origen permitido
+                'Access-Control-Allow-Headers': 'content-type, date, x-flow-origin, range',
+                'Access-Control-Allow-Methods': 'GET',
+            },
+            timeout: 15000 // Timeout de 15 segundos
+        });
         const { status } = req;
         callback(status === 200 || status == 302 || status === 304);
     } catch (error) {
