bf66734ce84a5d5195e9c92bff6517788f7bfb98
tricampeonsite <tricampeonsite@gmail.com>
2024-07-26 19:04:44 -0300

    restart extractSource_scrapping.js

--- api/libs/extractSource_scrapping.js
+++ api/libs/extractSource_scrapping.js
@@ -1,24 +1,15 @@
 import axios from "axios";
-import { load } from "cheerio";
+import { load } from "cheerio"
 import getUrl from "./getUrl.js";
-import https from 'https'
-import { config } from "dotenv";
-config();
 
-// @params url = String | recibe la url donde esta el stream
+const url = "https://la10hd.com/vivo/canal.php?stream=dsports";
+
+//@params url = String 
 export default async (url) => {
     try {
-        const response = await axios.get(`http://localhost:${process.env.PORT}/api/tricampeon/proxy/?url=${url}`, {
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-                "Accept-Language": "en-US,en;q=0.9",
-                "Connection": "keep-alive"
-            },
-            withCredentials: true
-        });
+        const response = await axios.get(url);
 
-        const $ = load(response.data.response);
+        const $ = load(response.data);
 
         const scriptsContent = [];
 
@@ -31,7 +22,8 @@ export default async (url) => {
         
         return getUrl(scriptsContent[0]);
     } catch (error) {
-        console.error('Error al hacer fetching en getSource_scrapping! Error: ', error);
+        console.error('Error al hacer fetching en getSource_scrapping! Error: ', error.code);
         return null;
     }
 }
+
